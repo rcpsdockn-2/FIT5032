e1917a3b54e45cfce119960e1c3ddadac58702c0
Haoyang Li <hlii0358@student.monash.edu>
2026-08-07 20:36:41 +1000

    Initial VueJS eFolio app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+import MyName from './components/MyName.vue'
+</script>
+
+<template>
+  <main class="welcome-page">
+    <section class="welcome-card" aria-labelledby="welcome-title">
+      <p class="eyebrow">FIT5032 · VueJS eFolio</p>
+      <h1 id="welcome-title">Welcome to my VueJS app</h1>
+      <MyName />
+    </section>
+  </main>
+</template>
---
+++ src/components/MyName.vue
@@ -0,0 +1,7 @@
+<script setup>
+const name = 'Haoyang Li'
+</script>
+
+<template>
+  <p class="my-name">My name is {{ name }}.</p>
+</template>
